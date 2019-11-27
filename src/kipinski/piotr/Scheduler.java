22c4841afbac0a1bdbcd3d161b078163105652e9
KipinskiPiotr <kipinski@student.agh.edu.pl>
2019-11-27 22:51:12 +0100

    Added blockingQueue and consumersQueue. Doesnt work tho

--- src/kipinski/piotr/Scheduler.java
+++ src/kipinski/piotr/Scheduler.java
@@ -1,30 +1,46 @@
 package kipinski.piotr;
 
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Scheduler extends Thread {
-    private final LinkedList<MethodRequest> queue = new LinkedList<>();
+    private final BlockingQueue<MethodRequest> blockingQueue = new LinkedBlockingQueue<>();
+    private final Queue<MethodRequest> consumersQueue = new LinkedList<>();
 
     public void run(){
         while (true) {
-            synchronized (queue) {
-                if (!queue.isEmpty()) {
-                    MethodRequest methodRequest = queue.pop();
-                    if (methodRequest.guard()) {
-                        methodRequest.execute();
-                        //System.out.println("Executed request");
-                    } else {
-                        enqueue(methodRequest);
-                    }
+            MethodRequest methodRequest = consumersQueue.poll();
+            if(methodRequest != null){
+                if (methodRequest.guard()) {
+                    methodRequest.execute();
+                    System.out.println("Executed request from consumersQueue");
+                } else {
+                    this.consumersQueue.add(methodRequest);
                 }
+            }
+            try {
+                methodRequest = blockingQueue.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if(methodRequest.guard()){
+                methodRequest.execute();
+                System.out.println("Executed request from blockingQueue");
+            }else if(methodRequest.getClass().isInstance(AddMethodRequest.class)){
+                this.consumersQueue.add(methodRequest);
+            }else{
+                enqueue(methodRequest);
             }
         }
     }
 
     void enqueue(MethodRequest methodRequest){
-        synchronized (queue) {
-            queue.add(methodRequest);
+        try {
+            blockingQueue.put(methodRequest);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        //System.out.println("Added request");
     }
 }
