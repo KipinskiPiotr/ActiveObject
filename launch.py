13e0888e39b6aeb19aee3a8217cee328fe08c4da
KipinskiPiotr <kipinski@student.agh.edu.pl>
2019-11-30 21:00:23 +0100

    Successfully saving data to CSV

--- launch.py
+++ launch.py
@@ -5,35 +5,9 @@ from subprocess import Popen, PIPE
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
 JAR_PATH = "target/active_object-1.0-SNAPSHOT-jar-with-dependencies.jar"
-
-
-def make_test(params):
-    process = Popen(["java", "-jar", JAR_PATH, json.dumps(params)], stdout=PIPE)
-    (output, err) = process.communicate()
-    exit_code = process.wait()
-
-    if exit_code != 0:
-        print("Error occured:")
-        print(output)
-        print(err)
-        sys.exit(1)
-    else:
-        pass
-
-    results = json.loads(output)
-    print(results)
-    return results['finishTime']
-
-
-def average_tests(params, no_tests=5):
-    result = 0
-    for i in range(no_tests):
-        result += make_test(params)
-    return result / no_tests
-
-
 starting_params = {'timedMode': False,
                    'testTime': 60000,
                    'bufferWorkTimeMultiplier': 0.01,
@@ -49,40 +23,58 @@ starting_params = {'timedMode': False,
                    'type': 'asynchronously'}
 
 
-def evaluate(params, sync_times, async_times, prod, no_tests=2):
+def save_to_csv(sync_data, async_data, file_name):
+    try:
+        with open(file_name, 'w') as f:
+            f.write('finishTime, productionsCounter, consumptionsCounter,'
+                    ' producersAdditionalWorkDone, consumersAdditionalWorkDone\n')
+            for v in sync_data:
+                f.write("%d, %d, %d, %d, %d\n" % (v[0], v[1], v[2], v[3], v[4]))
+            for v in async_data:
+                f.write("%d, %d, %d, %d, %d\n" % (v[0], v[1], v[2], v[3], v[4]))
+    except IOError:
+        print("I/O error")
+
+
+def run_java(params):
+    process = Popen(["java", "-jar", JAR_PATH, json.dumps(params)], stdout=PIPE)
+    (output, err) = process.communicate()
+    exit_code = process.wait()
+
+    if exit_code != 0:
+        print("Error occured:")
+        print(output)
+        print(err)
+        sys.exit(1)
+    else:
+        pass
+
+    results = json.loads(output)
+    print(results)
+    return list(results.values())
+
+
+def run_tests(params, sync_times, async_times, prod, no_tests=3):
     params['productionsPerProducer'] = prod
     params['consumptionsPerConsumer'] = prod
 
     params['type'] = 'asynchronously'
-    async_times.update({average_tests(params, no_tests)/1000: prod})  # we want seconds
+    for i in range(0, no_tests):
+        async_times.append(run_java(params))
 
     params['type'] = 'synchronously'
-    sync_times.update({average_tests(params, no_tests)/1000: prod})  # we want seconds
+    for i in range(0, no_tests):
+        sync_times.append(run_java(params))
 
 
-def productions_time_test(params):
-    sync_times = {}
-    async_times = {}
-    for prod in range(50, 500, 50):
-        evaluate(params, sync_times, async_times, prod)
-    for prod in range(500, 1000, 100):
-        evaluate(params, sync_times, async_times, prod)
-    for prod in range(1000, 2001, 200):
-        evaluate(params, sync_times, async_times, prod)
+def save_tests(params, file_name):
+    sync_times = []
+    async_times = []
+    for prod in range(100, 200, 100):
+        run_tests(params, sync_times, async_times, prod)
 
-    x, y = zip(*async_times.items())
-    plt.plot(x, y, label='Active Object')
-
-    x, y = zip(*sync_times.items())
-    plt.plot(x, y, label='Synchroniczny')
-
-    plt.xlim(xmin=0)
-    plt.ylim(ymin=0)
-    plt.xlabel("Czas: (s)")
-    plt.ylabel("Produkcje: (Konsumpcje)")
-    plt.legend()
-    plt.show()
+    save_to_csv(sync_times, async_times, file_name)
 
 
-productions_time_test(starting_params)
+save_tests(starting_params, 'data1.csv')
 print("Done!")
